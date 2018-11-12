948b43166f335743d3122fd970f891ead2a6c5fb
c1531722 <derosam@cardiff.ac.uk>
2018-11-12 13:06:45 +0400

    added lidar log

--- mqtt/logger.py
+++ mqtt/logger.py
@@ -1,10 +1,15 @@
 import paho.mqtt.client as mqtt
+import json
 
 f = open("log.txt", "w")
+ldrf = open("ldrlog.txt", "w")
 
 def onMessage(client, userdata, message):
     f.write(str(message.payload.decode("utf-8"))
-
+    msg = json.encode(str(message.payload.decode("utf-8")))
+    if msg["lidarreadings"]:
+        ldrf.write(msg["lidarreadings"])
+	
 client = mqtt.Client()
 client.on_message= onMessage
 client.connect("localhost")
